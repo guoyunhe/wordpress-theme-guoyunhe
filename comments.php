90bafbf5a257d2299d2b51054b8abbc530bb0e29
Guo Yunhe <i@guoyunhe.me>
2026-05-17 22:08:25 +0800

    移除不必要的SVG滤镜定义，优化页面性能

--- comments.php
+++ comments.php
@@ -6,15 +6,6 @@ if (post_password_required()) {
 
 <section id="comments" class="comments-area">
     <?php if (have_comments()) : ?>
-        <h2 class="comments-title">
-            <?php
-            printf(
-                esc_html(_nx('One comment', '%1$s comments', get_comments_number(), 'comments title', 'guoyunhe')),
-                esc_html(number_format_i18n(get_comments_number()))
-            );
-            ?>
-        </h2>
-
         <ol class="comment-list">
             <?php
             wp_list_comments([
@@ -26,10 +17,6 @@ if (post_password_required()) {
         </ol>
 
         <?php the_comments_navigation(); ?>
-    <?php endif; ?>
-
-    <?php if (!comments_open() && get_comments_number()) : ?>
-        <p class="no-comments"><?php esc_html_e('Comments are closed.', 'guoyunhe'); ?></p>
     <?php endif; ?>
 
     <?php comment_form(); ?>
--- footer.php
+++ footer.php
@@ -1,23 +1,5 @@
     </div><!-- #page -->
-    <svg class="crt-filter-defs" aria-hidden="true" focusable="false" width="0" height="0" xmlns="http://www.w3.org/2000/svg">
-        <defs>
-            <filter id="crt-barrel-distortion" x="-3%" y="-3%" width="106%" height="106%">
-                <feTurbulence type="fractalNoise" baseFrequency="0.65 0.003" numOctaves="1" seed="2" result="noise" />
-                <feDisplacementMap in="SourceGraphic" in2="noise" scale="1.4" xChannelSelector="R" yChannelSelector="G" result="distorted" />
-                <feComponentTransfer in="distorted" result="toneAdjusted">
-                    <feFuncR type="linear" slope="1.06" intercept="-0.03" />
-                    <feFuncG type="linear" slope="1.06" intercept="-0.03" />
-                    <feFuncB type="linear" slope="1.06" intercept="-0.03" />
-                </feComponentTransfer>
-                <feMorphology in="SourceAlpha" operator="dilate" radius="22" result="expandedAlpha" />
-                <feComposite in="expandedAlpha" in2="SourceAlpha" operator="out" result="edgeBand" />
-                <feFlood flood-color="#000000" flood-opacity="0.16" result="vignetteColor" />
-                <feComposite in="vignetteColor" in2="edgeBand" operator="in" result="vignette" />
-                <feBlend in="toneAdjusted" in2="vignette" mode="multiply" result="finalOutput" />
-                <feComposite in="finalOutput" in2="SourceGraphic" operator="over" />
-            </filter>
-        </defs>
-    </svg>
+
     <?php wp_footer(); ?>
 </body>
 </html>
